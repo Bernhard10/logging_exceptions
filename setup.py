fef80df9cb77b4f7d3e89dc6517a2f88864652d2
Bernhard Thiel <thiel@tbi.univie.ac.at>
2017-06-19 11:04:33 +0200

    Added README and setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='logging_exceptions',
+      version='0.1',
+      py_modules=['logging_exceptions'],
+      author="Bernhard C. Thiel",
+      author_email="thiel@tbi.univie.ac.at",
+      description="Self-logging exceptions: Attach log messages to exceptions and output them conditionally.",
+
+      )
