42fed738de0eac55b0dc81c27f65ceb4ede96d28
Bernhard Thiel <thiel@tbi.univie.ac.at>
2017-06-19 16:02:55 +0200

    Updated setup.py to include meta data

--- setup.py
+++ setup.py
@@ -1,10 +1,27 @@
 from setuptools import setup
 
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
 setup(name='logging_exceptions',
-      version='0.1.1',
+      version='0.1.2',
       py_modules=['logging_exceptions'],
       author="Bernhard C. Thiel",
       author_email="thiel@tbi.univie.ac.at",
       description="Self-logging exceptions: Attach log messages to exceptions and output them conditionally.",
+      long_description=long_description,
+      url='https://github.com/Bernhard10/logging_exceptions',
+      license='MIT',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.5'
+      ],
+      keywords='logging exceptions'
 
       )
